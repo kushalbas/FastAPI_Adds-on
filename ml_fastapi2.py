a56c7dc3ea0260afff1a8712857c9d7e6fede57d
kushalbas <143591123+kushalbas@users.noreply.github.com>
2024-10-19 01:18:29 +1100

    Update ml_fastapi2.py

--- ml_fastapi2.py
+++ ml_fastapi2.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.neural_network import MLPClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 import joblib
 import numpy as np
@@ -13,11 +13,11 @@ class EmailText(BaseModel):
 app = FastAPI()
 
 # Load the pre-trained model and vectorizer
-model: MultinomialNB = joblib.load('lightweight.pkl')
-vectorizer: TfidfVectorizer = joblib.load('ntransform.pkl')
+model: MLPClassifier = joblib.load('neuralTrained_model.pkl')
+vectorizer: TfidfVectorizer = joblib.load('neuraltransform.pkl')
 
 # Custom threshold (already defined)
-THRESHOLD = 0.48
+THRESHOLD = 0.5053181026693784
 
 @app.post("/predict/")
 async def predict(email: EmailText):
